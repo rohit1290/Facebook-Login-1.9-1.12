4e3c37d7c1860258f66d2a29737852a321dc15c8
TechBytes with Rohit <rohit1290@users.noreply.github.com>
2015-10-23 14:25:46 +0530

    Code Update: Facebook Posting

--- views/default/facebook_connect/login.php
+++ views/default/facebook_connect/login.php
@@ -2,18 +2,17 @@
 elgg_load_library('facebook');
 $fbData = array();
 $facebook = facebookservice_api();
-
-$helper = $facebook->getRedirectLoginHelper();  
+$helper = $facebook->getRedirectLoginHelper(); 
 $permissions = ['public_profile','email'];
+if(facebook_connect_allow_post_on_facebook()) { 
+$permissions = ['public_profile','email','publish_actions'];
+}
 $callback = elgg_get_site_url().'facebook_connect/login';
 $url = $helper->getLoginUrl($callback, $permissions);
-
 $img_url = elgg_get_site_url() . 'mod/facebook_connect/graphics/facebook_login.png';
-
 $login = <<<__HTML
 <div id="login_with_facebook">
 	<a href="$url" target="_self"><img src="$img_url" alt="Facebook" /></a>
 </div>
 __HTML;
-
 echo $login;
