65a338b66c0dfc44bde7e2aea0eae349513e510c
TechBytes with Rohit <rohit_1290@yahoo.com>
2016-01-04 23:39:31 +0530

    Added admin gatekeeper to test_param file

--- test_param.php
+++ test_param.php
@@ -1,28 +1,40 @@
+<style>
+body, html{
+padding: 5px;
+margin: 5px;
+}
+
+</style>
 <?php
 include("../../engine/start.php");
 
-echo elgg_echo('facebook_connect:consumer_key');
-echo "<br>";
+elgg_admin_gatekeeper();
+
+echo "<h2><u>Facebook Connect Parameters</u></h2>";
+
+echo "<ul>";
+echo "<b><li>".elgg_echo('facebook_connect:consumer_key')."</li></b>";
+
 echo elgg_get_plugin_setting('consumer_key', 'facebook_connect');
 echo "<br><br>";
 
-echo elgg_echo('facebook_connect:consumer_secret');
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:consumer_secret')."</li></b>";
+
 echo elgg_get_plugin_setting('consumer_secret', 'facebook_connect');
 echo "<br><br>";
 
-echo elgg_echo('facebook_connect:login');
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:login')."</li></b>";
+
 echo elgg_get_plugin_setting('sign_on', 'facebook_connect');
 echo "<br><br>";
 
-echo elgg_echo('facebook_connect:new_users');
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:new_users')."</li></b>";
+
 echo elgg_get_plugin_setting('new_users', 'facebook_connect');
 echo "<br><br>";
 
-echo elgg_echo('facebook_connect:post_onfb');
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:post_onfb')."</li></b>";
+
 echo elgg_get_plugin_setting('post_onfb', 'facebook_connect');
 echo "<br><br>";
 
@@ -32,8 +44,8 @@ $username = "Dummy User";
 $sitename = "Site Name";
 		
 		
-echo elgg_echo('facebook_connect:post_message',array("ABC Community"));
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:post_message',array("ABC Community"))."</li></b>";
+
 if(!empty(elgg_get_plugin_setting('post_message', 'facebook_connect'))){
 $temp_str = elgg_get_plugin_setting('post_message', 'facebook_connect');
 $temp_str2 = str_replace("%username%", "$username", $temp_str);
@@ -44,8 +56,8 @@ echo $username . ' just synched his/her facebook account with ' . $sitename;
 echo "<br><br>";
 
 
-echo elgg_echo('facebook_connect:post_img',array(elgg_get_site_url()));
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:post_img',array(elgg_get_site_url()))."</li></b>";
+
 if(!empty(elgg_get_plugin_setting('post_img', 'facebook_connect'))){
 echo "<img src='".elgg_get_plugin_setting('post_img', 'facebook_connect')."'>";
 }else{
@@ -53,8 +65,8 @@ echo "<img src='".$link."/_graphics/elgg_logo.png'>";
 }
 echo "<br><br>";
 
-echo elgg_echo('facebook_connect:post_descp',array("ABC Community"));
-echo "<br>";
+echo "<b><li>".elgg_echo('facebook_connect:post_descp',array("ABC Community"))."</li></b>";
+
 if(!empty(elgg_get_plugin_setting('post_descp', 'facebook_connect'))){
 $temp_str = elgg_get_plugin_setting('post_descp', 'facebook_connect');
 $temp_str2 = str_replace("%username%", "$username", $temp_str);
